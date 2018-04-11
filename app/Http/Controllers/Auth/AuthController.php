b12400c07f5c0b05d56bcbdf0cc24b282640a913
Manson Xasthur <mansonx13@gmail.com>
2018-04-11 18:46:09 +0200

    final

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -56,6 +56,7 @@ class AuthController extends Controller
             'email'    => $user->email,
             'provider' => $provider,
             'provider_id' => $user->id,
+            'avatar' => $user->avatar,
             'ref_id' => Str::uuid()
         ]);
     }
