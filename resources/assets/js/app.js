1bf736542b15b7d88f944170a40be02d1b190bcb
Manson Xasthur <mansonx13@gmail.com>
2018-05-03 12:18:25 +0200

    new changes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+/*Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 Vue.component('top-clients', require('./components/TopClients.vue'));
 Vue.component('main-jobs', require('./components/MainJobs.vue'));
@@ -45,4 +45,4 @@ Vue.component(
 
 const app = new Vue({
     el: '#app'
-});
+});*/
