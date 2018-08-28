9305667e33e1e2dc7f849995988e591d9b6302d8
Ahmed <Ahmed@gms.com>
2018-08-28 12:59:54 +0200

    LogOut

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -130,7 +130,19 @@
                                     <span class=" fa fa-angle-down"></span>
                                 </a>
                                 <ul class="dropdown-menu dropdown-usermenu pull-right">
-                                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                                    
+                                    <li>
+                                        <a class="dropdown-item" href=""
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            <i class="fa fa-sign-out pull-right"></i> {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </li>
+                                    
                                 </ul>
                             </li>
 
